715e96af144fbaec72f99e6ff189103322b2f98c
Ebj8 <ebj800@gmail.com>
2022-09-30 20:47:21 -0600

    cleaned data read in

--- sea_urchins.r
+++ sea_urchins.r
@@ -1,8 +1,10 @@
-library(tidymodels)  # for the parsnip package, along with the rest of tidymodels
+library(tidymodels) # for the parsnip package, along with the rest of tidymodels
 
 # Helper packages
-library(readr)       # for importing data
+library(readr) # for importing data
 library(broom.mixed) # for converting bayesian models to tidy tibbles
-library(dotwhisker)  # for visualizing regression results
+library(dotwhisker) # for visualizing regression results
 
-urchins <- read_csv("https://tidymodels.org/start/models/urchins.csv")
+urchins <- read_csv("https://tidymodels.org/start/models/urchins.csv") |>
+    setNames(c("food_regime", "initial_volume", "width")) |>
+    mutate(food_regime = factor(food_regime, levels = c("Initial", "Low", "High")))
